a2677437d96875dd022311a0effd6f00f493c31f
tianyuancuipan <tianyuancuipan@gmail.com>
2019-01-30 13:29:14 -0600

    Edit tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,5 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    get help_path
+    assert_select "title", full_title("Help")
   end
 end
