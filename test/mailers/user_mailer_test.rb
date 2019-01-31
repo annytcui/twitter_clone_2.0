bb44a05627ca81e893e38b667b13414ba598a404
tianyuancuipan <tianyuancuipan@gmail.com>
2019-01-31 13:38:58 -0600

    Add test for password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,4 +13,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
+
+  test "password_reset" do
+    user = users(:anny)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Reset Your Password - TRAILS Demo App", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@trailsdemoapp.com"], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 end
