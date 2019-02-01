9c938f66b02a759a5cd289d9b69c542969b082f2
tianyuancuipan <tianyuancuipan@gmail.com>
2019-01-31 20:20:56 -0600

    Add form validation

---
+++ app/views/static_pages/_validate_post.js
@@ -0,0 +1,15 @@
+$("#micropost-form").validate({
+  //error place
+  errorPlacement: function (error, element) {
+    error.insertAfter(element);
+    error.css("color", "#777");
+  },
+  //adding rules
+  rules: {
+  // content is required with max of 140
+    "micropost[content]":{
+      required: true,
+      maxlength: 140
+    }
+  }
+});
