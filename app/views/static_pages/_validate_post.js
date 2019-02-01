e20cf883badac2dbe2e07e08608a21be90a1e8bc
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-01 09:28:23 -0600

    Implement image upload

--- app/views/static_pages/_validate_post.js
+++ app/views/static_pages/_validate_post.js
@@ -13,3 +13,11 @@ $("#micropost-form").validate({
     }
   }
 });
+
+// Add file upload size validation
+$('#micropost_picture').bind('change', function() {
+  var size_in_megabytes = this.files[0].size/1024/1024;
+  if (size_in_megabytes > 5) {
+    alert('Maximum file size is 5MB. Please choose a smaller file.');
+  }
+});
