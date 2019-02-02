bdcbb47c833b90a2c890f3cbcb5c5bbf652d0c1f
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-02 14:57:28 -0600

    Edit comment and like actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,18 @@ class LikesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @like = current_user.likes.build(like_params)
+    if @like.save
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js { render action: "failed_create" }
+      end
+    end
   end
 
   def destroy
