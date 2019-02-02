bc6817d2fb8af081e1e479e2bcd2e874501f8096
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-01 20:40:39 -0600

    Test comment deletion

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,7 +5,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:anny)
     @micropost = microposts(:apple)
-    @comment = comments(:apple)
+    @comment = comments(:orange)
   end
 
   test "should redirect create when not logged in" do
@@ -21,5 +21,13 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
       delete micropost_comment_path(@comment.micropost, @comment)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong comment" do
+    log_in_as(@user)
+    assert_no_difference 'Comment.count' do
+      delete micropost_comment_path(@comment.micropost, @comment)
+    end
+    assert_redirected_to root_url
   end
 end
