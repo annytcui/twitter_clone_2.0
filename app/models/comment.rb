daa2b310d7cf51b1a5b7502155c43ce2ed2d28fa
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-02 09:16:56 -0600

    Add polymorphic associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :micropost
+  has_many :likes, as: :likeable, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :micropost_id, presence: true
   validates :user_id, presence: true
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
