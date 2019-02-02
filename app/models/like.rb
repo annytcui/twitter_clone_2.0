84b919a6dcdc5a737a903d701f0379aaeebc4eb7
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-02 09:29:35 -0600

    Add like validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
+  validates :user_id, presence: true
+  validates :likeable_id, presence: true
+  validates :likeable_type, presence: true
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:anny)
+    @micropost = microposts(:apple)
+    @like = @micropost.likes.build(user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @like.valid?
+  end
+
+  test "user id should be present" do
+    @like.user_id = nil
+    assert_not @like.valid?
+  end
+
+  test "likeable id should be present" do
+    @like.likeable_id = nil
+    assert_not @like.valid?
+  end
+
+  test "likeable type should be present" do
+    @like.likeable_type = nil
+    assert_not @like.valid?
+  end
 end
