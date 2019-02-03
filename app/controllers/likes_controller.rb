d17e22e7920d7600e3a9698a318c861785579e40
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-02 20:51:49 -0600

    Implement likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,8 @@ class LikesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-    @like = current_user.likes.build(like_params)
+    @like = current_user.likes.build(likeable_type: params[:likeable_type],
+                                      likeable_id: params[:likeable_id])
     if @like.save
       respond_to do |format|
         format.html { redirect_to request.referrer || root_url }
@@ -17,11 +18,11 @@ class LikesController < ApplicationController
   end
 
   def destroy
-  end
-
-  private
-
-    def like_params
-      params.require(:like).permit(:likeable_type, :likeable_id)
+    @like = Like.find_by(id: params[:id])
+    @like.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
     end
+  end
 end
