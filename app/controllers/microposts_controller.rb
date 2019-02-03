b5e01ff909eb601b3fb88efb17f83604773c70e1
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-03 14:22:41 -0600

    Add fallback for failed micropostion create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -12,8 +12,8 @@ class MicropostsController < ApplicationController
       end
     else
       # fallback
-      flash[:danger] = "Invalid micropost."
-      redirect_to root_url
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 15)
+      render 'static_pages/home'
     end
   end
 
