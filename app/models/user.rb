f964b82099907017fe704bcb290794c3e8c906ef
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-04 22:49:44 -0600

    Fix style error

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,12 +79,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto-feed
+  # Defines a feed
   def feed
     following_ids = "SELECT followed_id FROM relationships
                         WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",
-                        user_id: id)
+                    user_id: id)
   end
 
   # Follows a user.
