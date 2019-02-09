832bf832597feef9b91d2d9513597348715eede9
tianyuancuipan <tianyuancuipan@gmail.com>
2019-02-09 14:00:51 -0600

    Change title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "TRAILS Demo App"
+    base_title = "Twitter Clone"
     if page_title.empty?
       base_title
     else
